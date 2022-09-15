a3880012f24d6449455999d2f0667d3da985d552
muhammadzuama <108447400+muhammadzuama@users.noreply.github.com>
2022-09-15 08:05:50 +0700

    Update Tugas2.php

--- Tugas2.php
+++ Tugas2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas Jadi PHP</title>
+    <title>Tugas 2 PHP</title>
     <style>
         table,th, td {
         border: 1px solid black;
@@ -37,7 +37,7 @@
     ?>
     </table>
 <footer>
-    <p style="text-align:center ;">212410102032 - Muhammad Zuama Al Amin</p>
+    <p style="text-align:center ;">{212410102032} - {Muhammad Zuama Al Amin}</p>
 </footer>
 </body>
 </html>
